c1b38c0bcf6b4a00ede1dadc91f7da616325796c
GaelleBriet <gaelle.briet@protonmail.com>
2024-01-05 17:52:42 +0100

    oto rent, list view and details view

---
+++ oto_rent/lib/VehicleDetailPage.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class VehicleDetailPage extends StatelessWidget {
+  final Map<String, dynamic> vehicle;
+
+  const VehicleDetailPage({Key? key, required this.vehicle}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${vehicle['mark']} ${vehicle['model']}',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            )),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              vehicle['pic'],
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildDetailCard('Marque', '${vehicle['mark']}', Icons.directions_car),
+                  _buildDetailCard('Modèle', '${vehicle['model']}', Icons.category),
+                  _buildDetailCard('0 à 100', '${vehicle['zeroto100']}s', Icons.speed),
+                  _buildDetailCard('Classe énergétique', '${vehicle['energyClass']}', Icons.bolt),
+                  _buildDetailCard('Puissance', '${vehicle['power']} ch.', Icons.flash_on),
+                  _buildDetailCard('Vitesse maximale', '${vehicle['topspeed']} km/h.', Icons.speed),
+                  _buildDetailCard('Location', '${vehicle['location']}', Icons.location_on),
+                  _buildDetailCard('Prix', '${vehicle['price']}€', Icons.euro),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDetailCard(String label, String value, IconData icon) {
+    return Card(
+      elevation: 5,
+      shadowColor: Colors.black,
+      color: Colors.grey[800],
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: ListTile(
+        leading: Icon(icon, color: Colors.blue),
+        title: Text(label, style: const TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(value),
+      ),
+    );
+  }
+}
--- oto_rent/lib/main.dart
+++ oto_rent/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'VehicleDetailPage.dart';
+import 'data.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +11,133 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Oto Rent',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.dark(
+          primary: Colors.cyan,
+          secondary: Colors.blueAccent,
         ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            fontFamily: 'Roboto',
+          ),
+        ),
+        scaffoldBackgroundColor: Colors.grey[900],
+      ),
+      home: const MyHomePage(title: 'Oto Rent'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            title,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            )
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Paramètres',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Une snackBar comme ça pour rien...')));
+            },
+            color: Colors.white,
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: vehicules.length,
+        itemBuilder: (context, index) {
+          final vehicule = vehicules[index];
+          return Container(
+            height: 100, // Define a height for the Container
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              elevation: 5,
+              shadowColor: Colors.black,
+              color: Colors.grey[800],
+              margin: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage('${vehicule['pic']}'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        title: Text(
+                            '${vehicule['mark']} ${vehicule['model']}',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        subtitle: Text(
+                            'Location: ${vehicule['location']}',
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        trailing: Text(
+                            '${vehicule['price']}€',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.lightBlueAccent,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => VehicleDetailPage(vehicle: vehicule),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
